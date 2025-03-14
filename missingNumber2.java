370a6033d17d17f246749518ccaab999ba656b90
nagasundharam <nagasundharam553@gmail.com>
2025-03-14 14:05:54 +0530

    missingNumber using cyclic sort

---
+++ missingNumber2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int i = 0 ;
+
+        while (i < n){
+            int correct =nums[i];
+            if (nums[i] < n && nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            }
+            else {
+                i++;
+            }
+
+        }
+
+        for(int j = 0 ; j < n ;j++){
+            if ( nums[j] !=  j){
+                return j;
+            }
+        }
+        return n;
+
+  
+
+    
+
+       
+    }
+    public void swap(int[] nums, int start,int end){
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
