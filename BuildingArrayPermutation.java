b4d5722afdf07159baae57d1532506af1e54c0f3
nagasundharam <nagasundharam553@gmail.com>
2025-03-19 22:38:40 +0530

    Building arrray permutation problem solved

---
+++ BuildingArrayPermutation.java
@@ -0,0 +1,17 @@
+public class BuildingArrayPermutation {
+    public int[] buildArray(int[] nums) {
+
+        int n = nums.length;
+        int[] arr =new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+
+            arr[i] = nums[nums[i]];
+
+        }
+
+        return arr;
+        
+    }
+    
+}
