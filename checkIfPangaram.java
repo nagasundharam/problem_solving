a39b7ddc1e4e03f76f62fa83cfbf8e128c2f3060
nagasundharam <nagasundharam553@gmail.com>
2025-03-27 10:20:00 +0530

    Algorithm:Hashmap problemName:checkIfPangaram

---
+++ checkIfPangaram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class checkIfPangaram {
+
+    public boolean checkIfPangram(String sentence) {
+
+        int[] a = new int[26];
+        int n = sentence.length();
+
+        for(int i = 0 ; i  < n ; i++){
+            a[sentence.charAt(i) - 97]++;
+
+        }
+        System.out.println(Arrays.toString(a));
+        for(int i = 0 ; i < 26;i++){
+
+            if (a[i] == 0){
+                return false;
+            }
+        }
+        return true;
+
+         
+    }
+}
+
