1d6b0e30f0b2e6379ae2c94dc8a80864ffbda44e
nagasundharam <nagasundharam553@gmail.com>
2025-04-06 11:35:54 +0530

    Finding Even or Odd using bitwise operater

---
+++ bitwiseOperaterAnd.java
@@ -0,0 +1,12 @@
+public class bitwiseOperaterAnd {
+    public static void main(String args[]){
+        int n = 7;
+        if (n & 1 == 1){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+    
+}
