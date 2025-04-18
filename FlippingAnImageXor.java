129c09ce9cc0307b22c266629c15cae363cfe72d
nagasundharam <nagasundharam553@gmail.com>
2025-04-18 06:07:29 +0530

    Flipping an Image

---
+++ FlippingAnImageXor.java
@@ -0,0 +1,26 @@
+public class FlippingAnImageXor {
+   
+        public int[][] flipAndInvertImage(int[][] image) {
+    
+            int n = image.length;
+            int m = image[0].length;
+    
+       
+    
+            for(int i = 0; i < n  ; i++){
+                for(int j = 0 ; j  < (m+ 1)/2 ; j++){
+    
+                    int temp = image[i][j] ^ 1;
+                    image[i][j] = image[i][m-j-1] ^ 1;
+                    image[i][m-j-1] = temp;
+                    
+                    
+                    
+                }
+            }
+    
+            return image;
+            
+        }
+    }
+
